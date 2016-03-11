e718e0832f54b54907f6a09672cf042a3f087621
Lasse Diercks <lasse@jimdo.com>
2016-03-11 18:29:22 +0100

    add list of visual properties from external

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
+require('./node_modules/list-of-visual-css-properties/list.js');
 var css = require('css');
 var $ = require('jquery');
 
+var element = document.getElementsByClassName("jui-headline");
 
-var element2 = document.getElementsByClassName("jui-headline");
+var computedStyles = window.getComputedStyle(element[0],null);
 
-var computedStyles = window.getComputedStyle(element2[0],null);
-
-console.log(
-  computedStyles
-);
-
-
-
+for (var i=0; i < visualStyles.length; i++){
+  console.log(
+    visualStyles[i] + ':' + computedStyles[visualStyles[i]]
+  );
+}
 
 
 
-var visualStyles = [
-  'color',
-  'background-color',
-  'font-family'
-];
