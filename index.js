98607fb579c63be707a240214e1af7272928da2c
Lasse Diercks <lasse@jimdo.com>
2016-03-11 18:47:21 +0100

    set the font family in the svg via js :confetti_ball:

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('./node_modules/list-of-visual-css-properties/list.js');
 var css = require('css');
 var $ = require('jquery');
 
+var svg = $('#templateSvg')
+
 var element = document.getElementsByClassName("jui-headline");
 
 var computedStyles = window.getComputedStyle(element[0],null);
@@ -13,4 +15,19 @@ for (var i=0; i < visualStyles.length; i++){
 }
 
 
+console.log(svg)
 
+function paintSvg(){
+  return '<svg\
+    width="200" height="200"\
+    id="templateSvg"\
+    version="1.1"\
+    x="0px" y="0px"\
+    xml:space="preserve">\
+      <text transform="matrix(1 0 0 1 127.1431 137.9048)" font-family="' + computedStyles[visualStyles[2]] + '" font-size="12">\
+      I am a paragraph\
+      </text>\
+    </svg>';
+};
+
+$('body').append(paintSvg);
